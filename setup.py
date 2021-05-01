fb3d6799020f9487a6bc41013e24c28c2ff46d3e
Sam Purkis <sam.purkis@hotmail.co.uk>
2021-05-01 11:16:01 +0100

    add package files, license, setup.pu etc

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autocompile",
+    version="0.1.0",
+    author="Sam Purkis",
+    author_email="sam.purkis@hotmail.co.uk",
+    description="Speed up Python code that has well layed out type hints (works by converting the function to typed cython). Find more info at https://github.com/smpurkis/autocompile",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smpurkis/autocompile",
+    project_urls={
+        "Bug Tracker": "https://github.com/smpurkis/autocompile/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="autocompile"),
+    python_requires=">=3.6",
+)
